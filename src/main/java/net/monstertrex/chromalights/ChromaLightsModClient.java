3418d98ec02365931cbcc5ff21cc857f434e58ca
MonsterTrex <AdrianGabrysz0508@gmail.com>
2023-09-05 00:28:37 +0100

    Initial commit

---
+++ src/main/java/net/monstertrex/chromalights/ChromaLightsModClient.java
@@ -0,0 +1,10 @@
+package net.monstertrex.chromalights;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChromaLightsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/monstertrex/chromalights/ChromaLightsModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.monstertrex.chromalights;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChromaLightsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
